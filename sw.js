e7e702ce515bbf4ce40fa5835d7734b12238744b
zhoupan <zhoupan@cninnovatel.com>
2018-11-09 18:14:06 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
 var cacheStorageKey = 'minimal-pwa-11'
 var cacheList = [
-    '/',
     'index.html',
     'main.css',
     'github.png'
